f2496a34bd8253c12cbd08bf87295d68aa8d5b23
Shammi <shammi@test.com>
2025-04-30 23:54:44 +0530

    java-backend-email-assistant

---
+++ src/main/java/com/email/email_writer_sb/EmailWriterSbApplication.java
@@ -0,0 +1,14 @@
+package com.email.email_writer_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
+@SpringBootApplication
+public class EmailWriterSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailWriterSbApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/email/email_writer_sb/app/EmailRequest.java
@@ -0,0 +1,9 @@
+package com.email.email_writer_sb.app;
+
+import lombok.Data;
+
+@Data
+public class EmailRequest {
+    private String EmailContent;
+    private String Tone;
+}
